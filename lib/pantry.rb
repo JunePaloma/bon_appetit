c104cf5d86a275022749d74b0e155b205c6e58fa
JunePaloma <kborkoski@gmail.com>
2017-08-01 10:28:27 -0600

    Add first hour and a half of work thru middle of iteration 2

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,55 @@
+require "./lib/recipe"
+require 'pry'
 class Pantry
 
+  attr_reader :stock
+
+  def initialize
+    @stock = {}
+  end
+
+  def stock_check(item) #maybe refactor using find
+    quantity_count = 0
+    @stock.each do |stocked_item, quantity|
+      if stocked_item == item
+        quantity_count += quantity
+      end
+    end
+    quantity_count
+  end
+
+  def restock (item, quantity) #possible to use memoization to set the value of a key?
+    if @stock[item] == nil
+        @stock[item] = quantity
+    else
+    @stock[item] += quantity
+    end
+  end
+
+  def convert_units(recipe)
+    i = recipe.ingredients
+      new_i_hash = {}
+    i.each do |ingredient, amount|
+        measurement_hash = {}
+      if amount <= 1
+        amount = amount *1000.to_i
+        measurement_hash["quantity"] = amount
+        measurement_hash["units"] = "Milli-Units"
+      elsif amount >= 100
+          amount = amount/5
+          measurement_hash["quantity"] = amount
+          measurement_hash["units"] = "Centi-Units"
+      else
+        measurement_hash["quantity"] = amount
+        measurement_hash["units"] = "Universal-Units"
+      end
+      new_i_hash[ingredient] = measurement_hash
+    end
 end
+
+end
+
+    # key is the ingredient, value is a hash {quantity => num, units =>numb}
+    # iterate over the outer hash first, then iterate over the inner hash
+  #   If the recipe calls for more than 100 Units of an ingredient, convert it to Centi-units
+  #  If the recipe calls for less than 1 Units of an ingredient, convert it to Milli-units
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,48 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+ def test_it_exists
+   assert_instance_of Pantry, Pantry.new
+ end
 
+ def test_pantry_has_stock
+   p = Pantry.new
+   assert_equal ({}), p.stock
+ end
+
+ def test_we_can_check_stock
+   p = Pantry.new
+   assert_equal 0, p.stock_check("Cheese")
+ end
+
+ def test_pantry_can_be_restocked
+    p = Pantry.new
+    p.restock("Cheese", 10)
+    assert_equal 10, p.stock_check("Cheese")
+  end
+
+
+  def test_pantry_can_be_restocked_again_with_even_more_cheese
+    p = Pantry.new
+    p.restock("Cheese", 10)
+    p.restock("Cheese", 20)
+    assert_equal 30, p.stock_check("Cheese")
+  end
+
+  def test_we_can_convert_units
+    r = Recipe.new("Spicy Cheese Pizza")
+    r.add_ingredient("Cayenne Pepper", 0.025)
+    r.add_ingredient("Cheese", 75)
+    r.add_ingredient("Flour", 500)
+    pantry = Pantry.new
+
+
+    assert_equal ({"Cayenne Pepper" => {quantity: 25, units: "Milli-Units"},
+    "Cheese"         => {quantity: 75, units: "Universal Units"},
+    "Flour"          => {quantity: 5, units: "Centi-Units"}}), pantry.convert_units(r)
+
+  end
 end
